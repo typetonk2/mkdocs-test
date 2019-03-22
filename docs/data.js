885a140e439ef98e3816ccf42863242a253c246b
typetonk2 <typetonk2@gmail.com>
2019-03-22 11:05:21 +0900

    Add: chartjs example.

---
+++ docs/data.js
@@ -0,0 +1,23 @@
+<canvas id="myChart" width="400" height="400"></canvas>
+<script src="/js/chart.bundle.min.js"></script>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 10, 20, 30]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+</script>
